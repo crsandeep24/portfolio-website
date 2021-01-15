e420f7dc40a66488a28d640ef3cb37a7d3ddf2be
Sandeep Reddy <36433366+crsandeep24@users.noreply.github.com>
2021-01-14 23:18:35 -0500

    Added a Navbar component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello Sandeep</h1>
-      <h2>Hello Sandeep</h2>
-    </div>
-  );
+  return <Navbar />;
 }
