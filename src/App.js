5a1fd30d0217f41083b8af9d06536466f4398f34
Sandeep Reddy <36433366+crsandeep24@users.noreply.github.com>
2021-01-14 21:56:23 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Sandeep</h1>
+      <h2>Hello Sandeep</h2>
+    </div>
+  );
+}
