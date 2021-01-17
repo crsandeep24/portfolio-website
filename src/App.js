1d5660f56047146e81791336795a7de7cf5a707e
Sandeep Reddy <36433366+crsandeep24@users.noreply.github.com>
2021-01-16 20:22:43 -0500

    customized header, navbar using css3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 
 export default function App() {
   return (
     <>
+      <Particles
+        className="tsparticles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: "star",
+              stroke: {
+                width: 2,
+                color: 340744
+              }
+            }
+          }
+        }}
+      />
       <Navbar />
       <Header />
     </>
