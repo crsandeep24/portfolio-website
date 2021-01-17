7aace6525ef782276215888e5647418721ea7fba
Sandeep Reddy <36433366+crsandeep24@users.noreply.github.com>
2021-01-16 20:24:36 -0500

    added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,43 @@
 import React from "react";
 import "../App.css";
-import bgimage from "../wall-background.png";
+import bgimage from "../pexels-scott-webb-1029612.jpg";
+import Typed from "react-typed";
 
 const Header = () => {
   return (
-    <div className="header-wrapper">
-      <img src={bgimage} />
+    <div className="header-wraper">
+      <img src={bgimage} alt="put the logo here" />
       <div className="main-info">
-        <h1> Hello Sandeep Reddy </h1>
+        <h1>Howdy. I'm Sandeep Reddy. </h1>
+        <Typed
+          className="typed-text"
+          strings={[
+            "I'm a Web Developer",
+            "React focussed",
+            "Working for Kroger Technology"
+          ]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+        <a href="#" className="btn-main-offer">
+          Contact Me
+        </a>
       </div>
     </div>
+
+    // <main>
+    //   <img
+    //     src={bgimage}
+    //     alt="background-image"
+    //     className="absolute object-cover w-full h-full"
+    //   />
+    //   <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+    //     <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug">
+    //       Hello Sandeep
+    //     </h1>
+    //   </section>
+    // </main>
   );
 };
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-light bg-navbar">
       <div className="container">
         <a className="navbar-brand" href="#">
           <img className="logo" src={logo} alt="put the logo here" />
@@ -23,7 +23,7 @@ const Navbar = () => {
           <FontAwesomeIcon
             className="hamburger"
             icon={faBars}
-            style={{ color: "white" }}
+            style={{ color: "#fff" }}
           />
         </button>
 
